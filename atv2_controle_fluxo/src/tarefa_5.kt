d5c97fe53da7b40b0f4575e04e671a36b766a78f
Luiz Felipe <luiz.fma.up@gmail.com>
2024-08-15 21:00:19 -0300

    :recycle: refactor: Melhorando as resoluções

--- atv2_controle_fluxo/src/tarefa_5.kt
+++ atv2_controle_fluxo/src/tarefa_5.kt
@@ -1,6 +1,6 @@
 fun main() {
     val linhas = 4
-    val colunas =4
+    val colunas = 4
     var digito = 10.25f
     //colocar primeiro linhas e depois colunas caso o contrario sera uma matriz transposta
     val matriz: Array<Array<Float>> = Array(linhas) {Array(colunas) {0f}}
@@ -12,7 +12,7 @@ fun main() {
     }
 
     println("Quantidade de colunas: ${matriz[0].size}")
-    println("Quantidade de linhas: ${matriz.size}")
+    println("Quantidade de linhas: ${matriz.size}\n")
 
     for(i in 0 until linhas) {
         for(j in 0 until colunas) {
